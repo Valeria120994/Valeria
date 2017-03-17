8e433a7d03fdf4a14ef1e9c8893ecbd3f0f8dcb3
Валерия <lera_garieva@mail.ru>
2017-03-17 23:19:49 +0500

    до  вызова программы по расширению

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,8 @@ class Program
     static string[] extensions = { ".docx", ".doc",".txt",".jpeg"};
 
     static void Main(string[] args)
-        {
+    {
+        callProgram();
         if (args.Length > 0)
         {
             // чтение входных параметров
@@ -27,9 +28,9 @@ class Program
             else
             {
                 var fileInf = new FileInfo(path);
-                if (fileInf.Extension.Equals(".diplom")) 
+                if (fileInf.Extension.Equals(".diplom"))
                 {
-                    string Newpath = path.Remove(path.Length-7);
+                    string Newpath = path.Remove(path.Length - 7);
                     File.Copy(path, Newpath);
                     Process.Start("explorer.exe", Newpath); // открыть файл через проводник (проводник вызовет программу по умолчанию)
                     // отрыть файл
@@ -48,9 +49,18 @@ class Program
                     RegistryKey hklm = Registry.ClassesRoot;
                     RegistryKey hkSoftware = hklm.OpenSubKey("Word.Document.12");
                     RegistryKey hkMicrosoft = hkSoftware.OpenSubKey("shell");
-                    var a = hkMicrosoft.CreateSubKey("diplom2");
+                    var a = hkMicrosoft.CreateSubKey("diplom");
                     a.SetValue("icon", @"%SystemRoot%\system32\SHELL32.dll,47"); 
                     */
+    }
+
+    private static void callProgram()
+    {
+        RegistryKey hkcr = Registry.ClassesRoot; // открыли большую ветку HKEY_CLASSES_ROOT
+        RegistryKey hkrazdel = hkcr.OpenSubKey(".docx"); // открываем раздел с расширением
+        string value = (string)hkrazdel.GetValue("");
+        RegistryKey hkrazdel2 = hkcr.OpenSubKey(value).OpenSubKey("shell").OpenSubKey("Open").OpenSubKey("command");
+        string value2 = (string)hkrazdel2.GetValue("");
     }
 
     private static void ProcessFile(string path)
